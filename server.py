48f855b9bbd59ab5a60c51fefa5a17b0900175d8
den-rgb <den02_m@yahoo.com>
2022-11-25 23:05:36 +0000

    Added accessibilty from other devices + percentage similarity

--- server.py
+++ server.py
@@ -2,9 +2,13 @@ import eventlet
 from AttendanceProject import app
 import socketio
 import face_recognition as fr
+import socket
 
 sio = socketio.Server()
 appServer = socketio.WSGIApp(sio, app)
+
+hostname=socket.gethostname()
+IPAddr=socket.gethostbyname(hostname)
 
 @sio.event
 def connect(sid, environ):
@@ -19,4 +23,4 @@ def disconnect(sid):
     print('disconnect ', sid)
 
 if __name__ == '__main__':
-    eventlet.wsgi.server(eventlet.listen(('localhost', 5000)), appServer)
+    eventlet.wsgi.server(eventlet.listen((IPAddr, 5000)), appServer)
